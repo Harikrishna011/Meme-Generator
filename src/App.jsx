8ba415ec0879570371edad4d055cd4b0d7e4521e
Chaitanya <chaitanyapothala63@gmail.com>
2024-09-16 17:28:54 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from "./assets/components/Header"
+import Meme from "./assets/components/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
